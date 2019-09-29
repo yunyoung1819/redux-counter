aadd5cbeabce4736d712bf9d0bd40e357fec211a
yunyoung1819 <yunyoung1819@hanmail.net>
2019-09-29 14:12:31 +0900

    ActionsType.js 추가

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Counter from '../components/Counter';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Container
+                <Counter/>
             </div>
         );
     }
