663beb4e851d6bf689f77f08a5217d44d59b9a72
yunyoung1819 <yunyoung1819@hanmail.net>
2019-10-06 16:53:45 +0900

    redux-counter multi counter create:

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,16 +4,28 @@
 
 import * as types from './ActionTypes';
 
-export const increment = () => ({
-    type: types.INCREMENT
+export const create = (color) => ({
+    type: types.CREATE,
+    color
 });
 
-export const decrement = () => ({
-    type: types.DECREMENT
+export const remove = () => ({
+    type: types.REMOVE
+});
+
+export const increment = (index) => ({
+    type: types.INCREMENT,
+    index
+});
+
+export const decrement = (index) => ({
+    type: types.DECREMENT,
+    index
 });
 
 // 다른 액션 생성자들과 달리 파라미터를 갖고 있습니다.
-export const setColor = (color) => ({
+export const setColor = ({index, color}) => ({
     type: types.SET_COLOR,
+    index,
     color
 });
